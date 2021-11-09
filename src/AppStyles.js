6a7dbec8781145776a64762916fe1a0ec4991626
unknown <manoelprado.aecjr@gmail.com>
2021-11-09 15:44:03 -0300

    add styledcomponentes

---
+++ src/AppStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+
+export const SideBar = styled.div`
+  
+`;
+
+export const ContentArea = styled.div`
+  
+`;
+
