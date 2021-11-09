07917469d147e83f8aae9df2835e1e5e7072b988
unknown <manoelprado.aecjr@gmail.com>
2021-11-09 16:26:47 -0300

    create header sideBar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import {
   Container,
   SideBar,
+  SideBarHeader,
+  SideBarHeaderButtons,
+  HeaderButton,
+  Search,
+  ChatList,
   ContentArea,
 } from './styles';
 
@@ -10,10 +19,32 @@ export function Dashboard() {
   return (
     <Container>
       <SideBar>
-        dsssxs c c s
-      </SideBar>
-      <ContentArea>
+        <SideBarHeader>
+          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar"/>
+          <SideBarHeaderButtons>
+            <HeaderButton>
+              <DonutLargeIcon style={{color:'#919191'}}/>
+            </HeaderButton>
+            <HeaderButton>
+              <ChatIcon style={{color:'#919191'}}/>
+            </HeaderButton>
+            <HeaderButton>
+              <MoreVertIcon style={{color:'#919191'}}/>
+            </HeaderButton>
+          </SideBarHeaderButtons>
+        </SideBarHeader>
 
+        <Search>
+          ...
+        </Search>
+
+        <ChatList>
+          ...
+        </ChatList>
+      </SideBar>
+
+      <ContentArea>
+        ...
       </ContentArea>
     </Container>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,11 +1,54 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  height: 100vh;
+  background-color: #EDEDED;
 `;
 
 export const SideBar = styled.div`
-  
+  width: 35%;
+  max-width: 414px;
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid #DDDDDD;
+`;
+
+export const SideBarHeader = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 15px;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    cursor: pointer;
+  }
+`;
+
+export const SideBarHeaderButtons = styled.div`
+  display: flex;
+`;
+
+export const HeaderButton = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const Search = styled.div`
+
+`;
+export const ChatList = styled.div`
+
 `;
 
 export const ContentArea = styled.div`
