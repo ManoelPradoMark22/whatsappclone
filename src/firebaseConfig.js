decf05875c129527b16a87f066f40de5c4a58d8a
unknown <manoelprado.aecjr@gmail.com>
2021-11-11 18:39:23 -0300

    initial configs firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBEYAACIy-vHjt-XR760e8Mqvk2_8alsN0",
+  authDomain: "whatsappclone-5c937.firebaseapp.com",
+  projectId: "whatsappclone-5c937",
+  storageBucket: "whatsappclone-5c937.appspot.com",
+  messagingSenderId: "244250919428",
+  appId: "1:244250919428:web:861164f11859ddd859eb51"
+};
